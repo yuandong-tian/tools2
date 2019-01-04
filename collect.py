a7ad3aacd8e23aec2153c41a10b302359ac98076
Yuandong Tian <yuandong@fb.com>
2019-01-04 11:25:00 -0800

    Add graph drawing functionality

--- collect.py
+++ collect.py
@@ -1,39 +1,18 @@
-from multiprocessing import Pool
+import numpy as np
 import os
 import glob
 import json
 
-def parse_file(input):
-    import re
-    import json
+from .matchers import ParallelParser
+from collections import defaultdict
 
-    task_matcher = re.compile(r"\s*([0-9]+:|)\s*(args|stats):")
-
-    prefix, f = input
-    data = {}
-    arg_id = -1
-    for line in open(f):
-        m = task_matcher.match(line)
-        if not m: continue
-        task_id = m.group(1)
-        t = m.group(2)
-        if t == 'args':
-            arg_id += 1
-        key = "%s-%s-%d"% (prefix, task_id, arg_id)
-        if key not in data:
-            data[key] = dict(stats=[], args=[])
-        data[key][t].append(json.loads(line[len(m.group(0)):]))
-    return data
-
-def parse_files(inputs):
-    p = Pool(64)
-    data = p.map(parse_file, inputs)
-    # data = [ parse_file(input) for input in inputs ]
-
-    result = {}
-    for d in data:
-        result.update(d)
-    return result
+def get_log_fullname(root, job_id, job_idx, inputs, check=False):
+    f = "%d/output%d-0.log" % (job_id, job_idx)
+    prefix = "%d-%d" % (job_id, job_idx)
+    f = os.path.join(root, f)
+    if not check or os.path.exists(f):
+        inputs.append((prefix, f))
+        return True
 
 def get(job_names):
     root = "/checkpoint/yuandong/jobs"
@@ -51,47 +30,76 @@ def get_aml(job_names):
 
     inputs = []
     for job_name in job_names:
-        with open(os.path.join(json_root, job_name + ".json")) as f:
+        if not job_name.endswith(".json"):
+            job_name += ".json"
+
+        full_name = os.path.join(json_root, job_name)
+        if not os.path.exists(full_name):
+            print(f"{full_name} is not found!")
+            continue
+
+        with open(full_name) as f:
             jobs = json.load(f)
-        for job in jobs["jobs"]:
-            if not "id" in job: continue
-            f = "%d/output%d-0.log" % (job["id"], job["job_idx"])
-            prefix = "%d-%d" % (job["id"], job["job_idx"])
-            inputs.append((prefix, os.path.join(root, f)))
 
-    return parse_files(inputs)
-
-class AccuStats:
-    def __init__(self):
-        self.x = None
-        self.xsqr = None
-        self.counter = None
-
-    def feed(self, v):
-        l = v.shape[0]
-        if self.x is None:
-            self.x = v
-            self.xsqr = v * v
-            self.counter = np.ones(l)
+        if "jobs" in jobs:
+            for job in jobs["jobs"]:
+                if "id" in job:
+                    get_log_fullname(root, job["id"], job["job_idx"], inputs)
         else:
-            lmin = min(l, self.x.shape[0])
-            self.x[:lmin] += v[:lmin]
-            self.xsqr[:lmin] += v[:lmin] * v[:lmin]
-            self.counter[:lmin] += 1
+            # Treat jobs as a list.
+            for job_id in jobs:
+                job_idx = 0
+                while True:
+                    if not get_log_fullname(root, job_id, job_idx, inputs, check=True):
+                        break
+                    job_idx += 1
 
-    def get_mean_std(self):
-        mean = self.x / self.counter
-        std = np.sqrt(self.xsqr / self.counter - mean ** 2)
-        return mean, std
+    return inputs
 
+import pandas as pd
+
+def resample(df, at, method='linear'):
+    # previously it was the next line. Change it to take the union of new and old
+    # df = df.reindex(numpy.arange(df.index.min(), df.index.max(), 0.0005))
+    df = df.reindex( (df.index | at).unique() )
+    df = df.interpolate(method=method).loc[at]
+    return df
+
+def process(data, factors, xlabel, xs, ylabel, conditions={}):
+    # Merge runs whose factors are the same.
+    processed = dict()
+    for k, v in data.items():
+        args = v["args"][0]
+
+        key = "-".join([ "%s=%s" % (f, str(args[f])) for f in factors])
+        if any([args[f] != value for f, value in conditions.items()]):
+            continue
+
+        # print("Processing %s, key: %s" % (k, key))
+        df = pd.DataFrame(v["stats"]).set_index(xlabel).rename(columns={ ylabel : ylabel + "_" + k })
+        df = df[~df.index.duplicated(keep='first')]
+        df = resample(df, xs)
+        if key not in processed:
+            processed[key] = df
+        else:
+            processed[key] = processed[key].join(df)
+
+    result = dict()
+    for key, df in processed.items():
+        result[key] = pd.DataFrame(dict(mean=df.mean(axis=1), std=df.std(axis=1)))
+
+    return result, processed
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--json_prefix', type=str, help="Json file")
+    parser.add_argument('--json_files', type=str, nargs='+', help="Json files")
+    parser.add_argument("--matcher", type=str, default="standard_matcher", help="Match string")
 
     args = parser.parse_args()
 
-    data = get_aml([args.json_prefix])
+    parser = ParallelParser(args.matcher)
+    inputs = get_aml(args.json_files)
+    data = parser.parse(inputs)
     print(data.keys())
 
